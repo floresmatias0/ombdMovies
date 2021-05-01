571f3276e3dc08587bd11a2ec6af228fedab55f6
matias <floresmatias0@gmail.com>
2021-05-01 01:03:53 -0300

    add inicioV2

--- src/components/Inicio/Inicio.js
+++ src/components/Inicio/Inicio.js
@@ -1,9 +1,7 @@
 import React,{Component} from 'react' 
 
 export class Inicio extends Component{
-    constructor(props){
-        super(props),
-    }
+
     render(){
         return (
             <h1>Hola</h1>
