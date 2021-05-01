dcaf8fa5226c042b489bd5617a3ef210eb7736f6
matias <floresmatias0@gmail.com>
2021-05-01 02:37:00 -0300

    change routes

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
   return (
       <React.Fragment>
           <NavBar />
-          <Route exact path="/" component={Inicio} />
-          <Route exact path="/home" component={Buscador} />
+          <Route exact path="/home" component={Inicio} />
+          <Route exact path="/search" component={Buscador} />
           <Route path="/favs" component={Favoritos} />
           <Route path="/movie/:id" component={Movie} />
       </React.Fragment>
